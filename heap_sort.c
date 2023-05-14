daaa9e76e08b960a9d0cdadd7d08676ce26bcece
Clark <80899180+clarkyan7@users.noreply.github.com>
2023-05-14 14:22:38 +0800

    Update heap_sort.c

--- heap_sort.c
+++ heap_sort.c
@@ -1,2 +1,36 @@
 // heap sort
 
+void swap(int* a. int* b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+void heapify(int* arr, int size, int root) {
+  int largest = root;
+  int left = root * 2, right = root * 2 + 1;
+  
+  if (left < n && arr[left] > arr[largest]) {
+    largest = left;
+  }
+  
+  if (right < n && arr[right] > arr[largest]) {
+    largest = right;
+  }
+  
+  if (largest != root) {
+    swap(arr + root, arr + largest);
+    heapify(arr, size, largest);
+  }
+}
+
+void heap_sort(int* arr, int size) {
+  for (int i = size / 2 - 1; i >= 0; --i) {
+    heapify(arr, size, i);
+  }
+  
+  for (int i = size - 1; i >= 0; --i) {
+    swap(arr + i, arr);
+    heapify(arr, i, 0);
+  }
+}
